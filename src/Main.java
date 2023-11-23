a9d43a39c0e3f86ae48598384c9b44fbdc4a0114
tensept <vankaew58@gmail.com>
2023-11-23 13:45:42 +0700

    lab1 แก้

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main extends sort {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //input data
         Scanner s = new Scanner(System.in);
         Scanner p = new Scanner(System.in);
         System.out.print("How many numbers do you want to sort : ");
         int n = s.nextInt();
         System.out.print("Enter your array : ");
-        String num = p.nextLine();
-        String[] arrnum = num.split(" ");
-        //convert string[] to int []
-        int[] numbers = new int[arrnum.length];
-        for(int i=0;i<numbers.length;i++) numbers[i] = Integer.parseInt(arrnum[i]);
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = p.nextInt();
+        }
         //bubble sort
-        sortNum(numbers);
+        sortNum(num);
         //print
-        for(int i=0;i<n;i++){
-            System.out.println(numbers[i]);
+        for (int i = 0; i < n; i++) {
+            System.out.print(num[i] + " ");
         }
     }
 }
